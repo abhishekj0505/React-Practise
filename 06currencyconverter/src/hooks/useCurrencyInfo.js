87013ce3b5ad3d7fd63fc1742d29d696afd12bbb
abhishek jaiswal <abhishek.jaiswal5385@gmail.com>
2024-05-01 17:49:40 +0530

    ne update

---
+++ 06currencyconverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react"
+
+
+function useCurrencyInfo(currency) {
+    const [data, seatData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${inr}.json`)
+            .then((res) => res.json)
+            .then((res) => )
+    }, [])
+}
