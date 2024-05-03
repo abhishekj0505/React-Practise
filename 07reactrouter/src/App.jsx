cbd3da2dd885af77c61a47cf89aa31bb90c8e798
abhishek jaiswal <abhishek.jaiswal5385@gmail.com>
2024-05-03 14:51:20 +0530

    working on router

--- 07reactrouter/src/App.jsx
+++ 07reactrouter/src/App.jsx
@@ -2,18 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header/header'
-import Footer from './components/Footer/Footer'
-import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
-      <Footer/>
-      <Home/> 
+      <h1 className='bg-green-600 p-4'>React Router</h1>
     </>
   )
 }
