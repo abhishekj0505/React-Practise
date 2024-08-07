981857ca6f6b1c78e7d7cb7602cfa53ec231246d
abhishek jaiswal <abhishek.jaiswal5385@gmail.com>
2024-08-07 20:46:07 +0530

    changes in lines

--- 05password_generator/src/App.jsx
+++ 05password_generator/src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-6 my-8 text-orange-500 bg-gray-700'>
+      <div className='max-w-md mx-auto shadow-md rounded-lg px-4 py-6 my-8 text-orange-500 bg-gray-700'>
         <h1 className='text-white  text-center my-3'>
           Passowrd Generator
         </h1>
